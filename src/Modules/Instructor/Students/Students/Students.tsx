401167b295e24913f77b62bb52a96d4f14990640
mohamed reda <mohamed.reda86060@gmail.com>
2025-01-10 09:04:35 +0200

    rest of students

--- src/Modules/Instructor/Students/Students/Students.tsx
+++ src/Modules/Instructor/Students/Students/Students.tsx
@@ -5,6 +5,8 @@ import { axiosInstance, STUDENT_URLS } from '../../../../Services/URLS/INSTRUCTO
 
 export default function Students() {
   const [students, setStudents] = useState()
+  const [currentPage, setCurrentPage] = useState<number>(1);
+    const [groupsPerPage] = useState<number>(10);
   const getStudents = async () =>{
     try {
       const res = await axiosInstance.get(STUDENT_URLS.GET_ALL_STUDENTS);
@@ -19,7 +21,11 @@ export default function Students() {
   
    
   }, [])
-  
+  const indexOfLastGroup = currentPage * groupsPerPage;
+  const indexOfFirstGroup = indexOfLastGroup - groupsPerPage;
+  const currentStudents = students?.slice(indexOfFirstGroup, indexOfLastGroup);
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   return (
     <section className='w-full p-3'>
       <div className='flex items-end '>
@@ -32,11 +38,11 @@ export default function Students() {
       <div className='m-3 border border-gray-400 rounded-md p-4'>
         <h2>Students List</h2>
         <div className='grid grid-cols-1  lg:grid-cols-2'>
-          {students.map((student:any)=>(
+          {currentStudents?.map((student:any)=>(
             <div className='border border-gray-300 rounded-md p-2 m-2 flex justify-between items-center'>
             <div>
-            <h3>name</h3>
-            <p className='text-gray-500 text-sm'>class rank | avarage score</p>
+            <h3>{student.first_name} {student.last_name}</h3>
+            <p className='text-gray-500 text-sm'>gruop:{student?.group?.name}  | status: {student.status}</p>
             </div>
             <FaArrowCircleRight className='text-2xl'/>
           </div>
@@ -45,7 +51,23 @@ export default function Students() {
           
           
         </div>
-
+        <div className="text-center py-2">
+          <button
+            onClick={() => paginate(currentPage - 1)}
+            disabled={currentPage === 1}
+            className="px-4 py-2 mx-2"
+          >
+            ...
+          </button>
+          <span> {currentPage}</span>
+          <button
+            onClick={() => paginate(currentPage + 1)}
+            disabled={currentPage * groupsPerPage >= students?.length}
+            className="px-4 py-2 mx-2"
+          >
+            ...
+          </button>
+        </div>
       </div>
     </section>
   )
